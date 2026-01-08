b3f95bf9b50c8dbabe3f341925ee9d9a6ef30f85
Prarthana Purohit <prarthana.purohit03@gmail.com>
2026-01-08 20:33:42 +0530

    review request

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,13 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
-import { addRequests } from "../utils/requestSlice";
+import { addRequests, removeRequest } from "../utils/requestSlice";
 
 const Requests = () => {
   const dispatch = useDispatch();
   const requests = useSelector((store) => store.requests);
-  
+
+  //accept or reject request
+  const reviewRequest = async (status, id) => {
+    try{
+        const res = axios.post(BASE_URL + "/request/review/" + status + "/" + id, {}, {withCredentials: true});
+        dispatch(removeRequest(id));  //clear reviewed req from page
+    }catch(err){
+        console.log(err);
+    }
+  }
 
   const fetchRequests = async () => {
     try {
@@ -35,20 +44,20 @@ const Requests = () => {
       <h1 className="text-2xl text-bold">Pending requests</h1>
 
       {requests.map((req) => {
-        const { firstName, lastName, age, gender, about, photoUrl } =
+        const { firstName, lastName, age, gender, about, photoUrl,  } =
           req.fromUserId;
         return (
           <div key={req._id} className="w-full max-w-xl mx-auto my-4">
             <div className="card bg-base-300 shadow-md">
               <div className="card-body p-4">
-                <div className="flex items-center gap-4">
+                <div className="flex justify-between items-center gap-2">
                   {/* Profile photo */}
                   <img
                     src={photoUrl}
                     alt="profile"
-                    className="w-16 h-16 rounded-full object-cover"
+                    className="w-16 h-16 mx-2 rounded-full object-cover"
                   />
-
+        
                   {/* User info */}
                   <div className="flex flex-col">
                     <h2 className="text-lg font-semibold">
@@ -59,9 +68,22 @@ const Requests = () => {
                     </p>
                     <p className="text-sm opacity-70">{about}</p>
                   </div>
+
+                  <div className="">
+                    <button 
+                    onClick={() => reviewRequest("accepted", req._id)}  //this id is not user id, this is connection request id
+                    className="btn btn-soft btn-success mx-2">
+                      Accept
+                    </button>
+                    
+                    <button 
+                    onClick={() => reviewRequest("rejected", req._id)}
+                    className="btn btn-soft btn-error mx-2">Reject</button>
+                  </div>
+                  </div>
                 </div>
               </div>
-            </div>
+          
           </div>
         );
       })}
--- src/utils/requestSlice.js
+++ src/utils/requestSlice.js
@@ -5,10 +5,13 @@ const requestSlice = createSlice({
     initialState: [],
     reducers: {
         addRequests: (state, action) => action.payload,
-        removeRequests: () => null,
+        removeRequest: (state,action) => {
+            const newArray = state.filter((r) => r._id !== action.payload);
+            return newArray;  //remove only one id 
+        },
     }
 })
 
 export default requestSlice.reducer;
 
-export const {addRequests, removeRequests} = requestSlice.actions;
+export const {addRequests, removeRequest} = requestSlice.actions;
